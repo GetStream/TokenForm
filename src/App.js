d14efca75495413dbc4281258f6ef73b18649030
Jaap <jaap@getstream.io>
2020-03-25 15:27:55 +0100

    update styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import { Fragment, h } from 'preact';
 import useJWTGenerator from './useJWTGenerator';
 
-const renderJWT = (jwt, userID) => jwt && userID ? <pre id="jwt"><span>{jwt}</span></pre> : <p id="warning" class="show">Please enter a Stream User ID</p>;
+const renderJWT = (jwt, userID) => {
+	if (jwt && userID) {
+		return (
+			<pre id="jwt">{jwt.split('.').map((item, i, a) => <span>{item}{i < a.length - 1  ? '.': ''}</span>)}</pre>
+		)
+	} else {
+		return <p id="warning" class="show">Please enter a Stream User ID</p>
+	}
+} 
 
 const App = ({container}) => {
 	const [jwt, { userID, secret, handleSecretChange, handleUserIDChange, options }] = useJWTGenerator(container);
@@ -10,7 +18,7 @@ const App = ({container}) => {
 		<Fragment>
 			<div class="header">
 				<div class="inputs">
-					<input id="secret" value={secret} onChange={handleSecretChange} />
+					<input id="secret" value={secret} placeholder="Your secret" onChange={handleSecretChange} />
 					<input id="user_id" placeholder="User ID" value={userID} onInput={handleUserIDChange} />
 				</div>
 			</div>
